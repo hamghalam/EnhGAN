04c5513cbba224c650e4b279fde82acdf28d59a0
yilmaz <72473367+yilmaz7@users.noreply.github.com>
2020-10-24 09:28:23 -0400

    Update Enhancement_GAN.py

--- Enhancement_GAN.py
+++ Enhancement_GAN.py
@@ -303,14 +303,18 @@ def enhance(config_file):
         print(volume_name_enhanced)
     
     #print(data_num)
-
-if __name__ == '__main__':
+ 
+def main():
     if(len(sys.argv) != 2):
         print('Number of arguments should be 2. e.g.')
         print('python Enhancement_GAN.py config17/train_wt_ax.txt')
-        sys.exit()#
+        sys.exit() #to safely exit & cleanup memory
     config_file = str(sys.argv[1])
     assert(os.path.isfile(config_file))
     enhance(config_file)
+    
+if __name__ == '__main__':main()
+    
+#END of script
     
     
